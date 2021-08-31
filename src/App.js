825accd2ca5b941ae008570a828ab135aead85c9
Harshit Pratap Singh <singhharshitpratap@gmail.com>
2021-08-31 21:27:28 +0000

    react component 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { dishes } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
+class App extends Component() {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      dishes:dishes
+    };
+  }
+  render() {
+
   return (
     <div className="App">
-     <Navbar dark color='primary'>
-       <div className="container">
-         <NavbarBrand href='/'>
-           Restorante Con fusion
-         </NavbarBrand>
-       </div>
-     </Navbar>
+      <Navbar dark color='danger'>
+        <div className="container">
+          <NavbarBrand href='/'>
+            Restorante Con Fusion
+          </NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
