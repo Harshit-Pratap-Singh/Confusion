6c1ca2e68ba3d8054072da1f6e7e1ac701647e94
Harshit Pratap Singh <singhharshitpratap@gmail.com>
2021-09-07 20:00:44 +0000

    SPA Part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,11 @@ import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
 import { Redirect, Route, Switch } from 'react-router';
+import Contact from './Contact';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
+
 
 class Main extends Component {
   constructor(props)
@@ -13,17 +18,21 @@ class Main extends Component {
     super(props);
     this.state={
       dishes: dishes,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
   };
 }
      
   render() {
-    const HomePage=() => <Home/>
+    const HomePage=() => <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]} leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
   return (
     <div >
       <Header/>
      <Switch>
      <Route path='/home' component={HomePage}/>
      <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>} />
+     <Route path='/contactus' component={Contact}/>
     <Redirect to='/home'/>
      </Switch>
       <Footer/>
